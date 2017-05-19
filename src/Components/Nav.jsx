069a63365948d2ff2465225a6217907187c4a2c5
Phillip Eade <Phillip Eade>
2017-05-19 19:36:43 +1000

    start

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -13,14 +13,14 @@ class Nav extends Component{
 
     <div className="navbar-header">
 
-      <Link className="navbar-brand" activeClassName="navbar-brand" to="/home">React Giphy</Link>
+      <Link className="navbar-brand" activeClassName="navbar-brand" to="/">React Giphy</Link>
     </div>
 
 
     <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul className="nav navbar-nav">
-        <li><Link to="/" activeClassName="active " >Search</Link ></li>
-        <li><Link to="/trending" activeClassName="active ">Trending</Link ></li>
+        <li><Link to="/" activeClassName="active" >Search</Link ></li>
+        <li><Link to="/trending" activeClassName="active">Trending</Link ></li>
 
       </ul>
     </div>
